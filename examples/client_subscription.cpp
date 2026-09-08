aed3836c55dde0f952223dd4f012e7044b082413
Dzc <3022353501@qq.com>
2026-09-08 19:10:44 +0800

    fix: create subscriptions asynchronously in session callback (#725)

--- examples/client_subscription.cpp
+++ examples/client_subscription.cpp
@@ -5,7 +5,8 @@
 #include <thread>
 
 #include <open62541pp/client.hpp>
-#include <open62541pp/subscription.hpp>
+#include <open62541pp/services/monitoreditem.hpp>
+#include <open62541pp/services/subscription.hpp>
 
 inline static std::atomic<bool> isRunning = true;  // NOLINT(*global-variables)
 
@@ -23,37 +24,48 @@ int main() {
     // items are deleted. This approach with the state callback assures, that the subscriptions are
     // recreated whenever the client reconnects to the server.
     client.onSessionActivated([&] {
-        opcua::Subscription sub{client};
-
-        // Modify and delete the subscription via the returned Subscription<T> object
+        // Synchronous service calls are not allowed from a state callback because it is invoked
+        // while the client's event loop is running. Use the asynchronous service API instead.
         opcua::SubscriptionParameters subscriptionParameters{};
         subscriptionParameters.publishingInterval = 1000.0;
-        sub.setSubscriptionParameters(subscriptionParameters);
-        sub.setPublishingMode(true);
-        // sub.deleteSubscription();
-
-        // Create a monitored item within the subscription for data change notifications
-        auto mon = sub.subscribeDataChange(
-            opcua::VariableId::Server_ServerStatus_CurrentTime,  // monitored node id
-            opcua::AttributeId::Value,  // monitored attribute
-            [&](opcua::IntegerId subId, opcua::IntegerId monId, const opcua::DataValue& dv) {
-                opcua::MonitoredItem item{client, subId, monId};
+        opcua::services::createSubscriptionAsync(
+            client,
+            subscriptionParameters,
+            true,  // publishingEnabled
+            {},  // statusChangeCallback
+            {},  // deleteCallback
+            [&](opcua::CreateSubscriptionResponse& response) {
                 std::cout
-                    << "Data change notification:\n"
-                    << "- subscription id:   " << item.subscriptionId() << "\n"
-                    << "- monitored item id: " << item.monitoredItemId() << "\n"
-                    << "- node id:           " << opcua::toString(item.nodeId()) << "\n"
-                    << "- attribute id:      " << static_cast<int>(item.attributeId()) << "\n"
-                    << "- value:             " << opcua::toString(dv) << "\n";
+                    << "Subscription created:\n"
+                    << "- status code: " << response.responseHeader().serviceResult() << "\n"
+                    << "- subscription id: " << response.subscriptionId() << std::endl;
+
+                opcua::services::createMonitoredItemDataChangeAsync(
+                    client,
+                    response.subscriptionId(),
+                    opcua::ReadValueId{
+                        opcua::VariableId::Server_ServerStatus_CurrentTime,
+                        opcua::AttributeId::Value
+                    },
+                    opcua::MonitoringMode::Reporting,
+                    opcua::MonitoringParametersEx{},  // default monitoring parameters
+                    [](opcua::IntegerId subId, opcua::IntegerId monId, const opcua::DataValue& dv) {
+                        std::cout
+                            << "Data change notification:\n"
+                            << "- subscription id:   " << subId << "\n"
+                            << "- monitored item id: " << monId << "\n"
+                            << "- value:             " << opcua::toString(dv) << std::endl;
+                    },
+                    {},  // deleteCallback
+                    [](opcua::MonitoredItemCreateResult& result) {
+                        std::cout
+                            << "Monitored item created:\n"
+                            << "- status code: " << result.statusCode() << "\n"
+                            << "- monitored item id: " << result.monitoredItemId() << std::endl;
+                    }
+                );
             }
         );
-
-        // Modify and delete the monitored item via the returned MonitoredItem<T> object
-        opcua::MonitoringParametersEx monitoringParameters{};
-        monitoringParameters.samplingInterval = 100.0;
-        mon.setMonitoringParameters(monitoringParameters);
-        mon.setMonitoringMode(opcua::MonitoringMode::Reporting);
-        // mon.deleteMonitoredItem();
     });
 
     std::signal(SIGINT, signalHandler);  // NOLINT
